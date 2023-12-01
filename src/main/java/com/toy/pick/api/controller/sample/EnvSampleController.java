db62f48248f6777b193d36178423b09bc06a2122
ha9011 <gkehddnjs90@gmail.com>
2023-12-02 07:55:28 +0900

    java 17 -> 11 , gradle 설정 변경

--- src/main/java/com/toy/pick/api/controller/sample/EnvSampleController.java
+++ src/main/java/com/toy/pick/api/controller/sample/EnvSampleController.java
@@ -1,13 +1,8 @@
 package com.toy.pick.api.controller.sample;
 
-import com.toy.pick.api.ApiResponse;
-import com.toy.pick.api.controller.sample.dto.request.GetSampleReqDto;
-import com.toy.pick.api.controller.sample.dto.response.GetSampleResDto;
-import com.toy.pick.api.service.sample.SampleService;
-import jakarta.validation.Valid;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- src/main/java/com/toy/pick/api/controller/sample/dto/request/GetSampleReqDto.java
+++ src/main/java/com/toy/pick/api/controller/sample/dto/request/GetSampleReqDto.java
@@ -1,6 +1,5 @@
 package com.toy.pick.api.controller.sample.dto.request;
 
-import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
