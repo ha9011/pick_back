7bcaade928a8bc875d12338655f848f91c0c03d4
ha9011 <gkehddnjs90@gmail.com>
2023-12-09 09:19:31 +0900

    spring & java 버전 변경, 리펙토링

--- src/main/java/com/toy/pick/api/controller/sample/SampleController.java
+++ src/main/java/com/toy/pick/api/controller/sample/SampleController.java
@@ -4,12 +4,12 @@ import com.toy.pick.api.ApiResponse;
 import com.toy.pick.api.controller.sample.dto.request.GetSampleReqDto;
 import com.toy.pick.api.controller.sample.dto.response.GetSampleResDto;
 import com.toy.pick.api.service.sample.SampleService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
--- src/main/java/com/toy/pick/domain/sample/Sample.java
+++ src/main/java/com/toy/pick/domain/sample/Sample.java
@@ -1,12 +1,12 @@
 package com.toy.pick.domain.sample;
 
 import com.toy.pick.domain.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Getter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 
 @Entity
 @Getter
