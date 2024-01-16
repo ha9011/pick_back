eaee7335690b10a4d6b679605b447516c7139fee
ha9011 <gkehddnjs90@gmail.com>
2024-01-16 11:09:27 +0900

    user entity 기본생성자 추가

--- src/main/java/com/toy/pick/domain/user/User.java
+++ src/main/java/com/toy/pick/domain/user/User.java
@@ -4,8 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
 
     @Id
@@ -18,6 +21,7 @@ public class User {
 
     private String name; // 닉네임
     private String refreshToken;
+
 
 
     public void updateRefreshToken(String refreshToken){
