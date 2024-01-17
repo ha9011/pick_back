0d436291c28f88d390ff3fe3a6a318534f500252
ha9011 <gkehddnjs90@gmail.com>
2024-01-17 17:12:13 +0900

    테스터계정 컨트롤 생성, sns네이버 로그인 api 키 변경

---
+++ src/main/java/com/toy/pick/api/controller/tester/TesterController.java
@@ -0,0 +1,10 @@
+package com.toy.pick.api.controller.tester;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class TesterController {
+
+}
